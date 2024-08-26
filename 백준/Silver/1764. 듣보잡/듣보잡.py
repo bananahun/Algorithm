b458c2abc4bf8c2fd720ac4bfa3008e56622fe51
bananahun <156387942+bananahun@users.noreply.github.com>
2024-08-26 15:54:45 +0900

    [Silver IV] Title: 듣보잡, Time: 2388 ms, Memory: 43924 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+arr1= set()
+arr2= set()
+
+for _ in range(N):
+    arr1.add(input())
+for _ in range(M):
+    arr2.add(input())
+
+result = sorted(set(arr1&arr2))
+
+print(len(result))
+for name in result:
+    print(name)
