42b7a166557b5a2fe6eb601b37a11d9d01fca3ac
bananahun <156387942+bananahun@users.noreply.github.com>
2025-02-01 20:57:40 +0900

    [Silver II] Title: 마인크래프트, Time: 228 ms, Memory: 35076 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,53 +1,33 @@
 import sys
+from collections import Counter
+
 input = sys.stdin.readline
 
 N, M, B = map(int, input().split())
-lst = []
-min_num = 256
-avg = 0
+block_dict = Counter()
+
+min_num, max_num = 256, 0
 
 for _ in range(N):
-    check_lst = list(map(int, input().split()))
-    lst.append(check_lst)
-    min_num = min(min_num, min(check_lst))
-    avg += sum(check_lst)
+    row = list(map(int, input().split()))
+    for height in row:
+        block_dict[height] += 1
+        min_num = min(min_num, height)
+        max_num = max(max_num, height)
 
-check = int((avg / (N * M)) + 0.5)
+res_time = float('inf')
+res_height = 0
 
-res_1 = 10**9
-res_2 = 0
-res_time, res_height = 10**9, 0
+# 전체 블록 개수
+total_blocks = sum(height * count for height, count in block_dict.items())
 
-def check_f():
-    global res_1, res_2
+for target in range(min_num, max_num + 1):
+    remove_blocks = sum((height - target) * block_dict[height] for height in block_dict if height > target)
+    add_blocks = sum((target - height) * block_dict[height] for height in block_dict if height < target)
 
-    for target in range(min_num, 257):
-        minus_num, plus_num = 0, 0
+    if remove_blocks + B >= add_blocks:
+        time = remove_blocks * 2 + add_blocks
+        if time < res_time or (time == res_time and target > res_height):
+            res_time, res_height = time, target
 
-        for i in range(N):
-            for j in range(M):
-                check_num = lst[i][j] - target
-                if check_num > 0:
-                    minus_num += check_num
-                else:
-                    plus_num -= check_num
-
-        if minus_num + B >= plus_num:
-            time = minus_num * 2 + plus_num
-            if time < res_1 or (time == res_1 and target > res_height):
-                res_1, res_height = time, target
-
-    for i in range(N):
-        for j in range(M):
-            res_2 += (lst[i][j] - min_num) * 2
-
-
-    if res_1 == res_2:
-        print(res_1, max(res_height, min_num))
-    else:
-        if res_1 < res_2:
-            print(res_1, res_height)
-        else:
-            print(res_2, min_num)
-
-check_f()
+print(res_time, res_height)
