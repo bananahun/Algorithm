b088197b9551a59d39db5adab0f14f53d7801965
bananahun <156387942+bananahun@users.noreply.github.com>
2025-02-12 17:32:43 +0900

    [Silver III] Title: N과 M (3), Time: 1856 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+
+n, m = map(int, input().split())
+res = []
+# visited = [False] * (n+1)
+
+def f():
+    if len(res) == m:
+        print(*res)
+        return
+    for i in range(1, n+1):
+        # if visited[i] == False:
+        # visited[i] = True
+        res.append(i)
+        f()
+        res.pop()
+        # visited[i] = False
+f()
